0628502b0568cbafbf965a0e5f5e19132e1cbe60
Aditya-Garg10 <153644983+Aditya-Garg10@users.noreply.github.com>
2024-03-23 15:54:36 +0530

    Update script.js
    removing extra nonuseful things

--- script.js
+++ script.js
@@ -80,55 +80,6 @@ for (let pattern of array1) {
 
 
 
-
-
-
-
-
-
-
-/*
-userPlay(userChoice)
-
-const userPlay = (userChoice,) => {
-
-    if (userChoice === "box1"){
-        const box1 = document.querySelector("#box1");
-        box1.innerText = "X";
-    }
-    else if (userChoice === "box2"){
-        const box2 = document.querySelector("#box2");
-        box2.innerText = "X";
-    }
-    else if (userChoice === "box3"){
-        const box3 = document.querySelector("#box3");
-        box3.innerText = "X";
-    }else if (userChoice === "box4"){
-        const box4 = document.querySelector("#box4");
-        box4.innerText = "X";
-    }
-    else if (userChoice === "box5"){
-        const box5 = document.querySelector("#box5");
-        box5.innerText = "X";
-    }
-    else if (userChoice === "box6"){
-        const box6 = document.querySelector("#box6");
-        box6.innerText = "X";
-    }
-    else if (userChoice === "box7"){
-        const box7 = document.querySelector("#box7");
-        box7.innerText = "X";
-    }
-    else if (userChoice === "box8"){
-        const box8 = document.querySelector("#box8");
-        box8.innerText = "X";
-    }
-    else{
-        const box9 = document.querySelector("#box9");
-        box9.innerText = "X";
-    } */
-    
-
  
     
 
